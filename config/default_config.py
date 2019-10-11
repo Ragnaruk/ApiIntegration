83aec956ead82a52e658926cc35d5d6a0930fc63
Ragnaruk <ima1365@me.com>
2019-10-11 20:18:51 +0300

    Changed default log levels

--- config/default_config.py
+++ config/default_config.py
@@ -14,12 +14,12 @@ path_logs_directory = path_working_directory / 'logs'
 path_logs_directory.mkdir(parents=True, exist_ok=True)
 
 # Scenario: Sync Groups and Zulip
-zulip_sync_logging_level = ERROR
+zulip_sync_logging_level = INFO
 zulip_sync_google_domain = ''
 zulip_sync_mandatory_members = ['']
 zulip_sync_sleep_time = 60 * 10
 
 # Scenario: Create Google Groups
-google_groups_logging_level = ERROR
+google_groups_logging_level = INFO
 google_groups_google_domain = ''
 google_groups_user_filter_query = ''
