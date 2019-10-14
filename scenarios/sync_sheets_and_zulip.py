0e281c1a2fcc433bad480cf3851d5bbb9f78c8f0
Ragnaruk <ima1365@me.com>
2019-10-14 22:59:02 +0300

    Improved exception logging Added get_client call Added search for similar stream names in case of renaming

--- scenarios/sync_sheets_and_zulip.py
+++ scenarios/sync_sheets_and_zulip.py
@@ -3,7 +3,7 @@ from time import sleep
 
 from logs.logging import get_logger
 from api_google.google_api_sheets import get_sheets_service, get_muiltiple_ranges
-from api_zulip.zulip_api import create_stream, get_all_users
+from api_zulip.zulip_api import get_client, create_stream, get_all_users, get_all_streams
 from config.config import sync_sheets_and_zulip, path_data_directory
 
 
@@ -31,6 +31,7 @@ def main():
 
         try:
             service = get_sheets_service()
+            client = get_client()
 
             ranges = get_muiltiple_ranges(
                 service,
@@ -45,19 +46,46 @@ def main():
 
             [logger.debug(x) for x in ranges]
 
-            zulip_user_emails = set([member['email'] for member in get_all_users()['members']])
+            zulip_user_emails = set(
+                [member['email'] for member in get_all_users(client)['members']]
+            )
+            zulip_stream_names = sorted(
+                [stream['name'] for stream in get_all_streams(client)['streams']]
+            )
+
+            logger.debug(zulip_stream_names)
 
             for i in [0, 3]:
                 for j in range(len(ranges[i]['values'])):
                     # If name and email of the leader both exist
                     if ranges[i + 0]['values'][j] and ranges[i + 2]['values'][j]:
-                        name = ranges[i + 0]['values'][j][0]
+                        id = name = ranges[i + 0]['values'][j][0]
                         description = \
                             ranges[i + 1]['values'][j][0] if ranges[i + 1]['values'][j] else ''
                         leader_email = ranges[i + 2]['values'][j][0]
 
-                        if name not in synced_users:
-                            synced_users[name] = set()
+                        if id not in synced_users:
+                            synced_users[id] = set()
+
+                        # Checking for renamed streams (streams that start with value of name)
+                        similar_stream_names = [x for x in zulip_stream_names if x.startswith(name)]
+
+                        logger.debug('similar_stream_names: %s', similar_stream_names)
+
+                        if len(similar_stream_names) > 1:
+                            more_similar_stream_names = \
+                                [x for x in similar_stream_names if x.startswith(name + ' ')]
+
+                            logger.debug('more_similar_stream_names: %s', more_similar_stream_names)
+
+                            if len(more_similar_stream_names) == 1:
+                                name = more_similar_stream_names[0]
+                            else:
+                                logger.error('Several streams starting with: %s', name)
+                                logger.error('Without space: %s', similar_stream_names)
+                                logger.error('With space: %s', more_similar_stream_names)
+                        elif len(similar_stream_names) == 1:
+                            name = similar_stream_names[0]
 
                         # Add leader's email if they are registered in Zulip
                         member_emails = set()
@@ -66,18 +94,19 @@ def main():
 
                         # Add mandatory members and substract already synced emails
                         member_emails |= set(sync_sheets_and_zulip['mandatory_members'])
-                        member_emails -= synced_users[name]
+                        member_emails -= synced_users[id]
 
                         # Update synced users
-                        synced_users[name] |= member_emails
+                        synced_users[id] |= member_emails
 
                         member_emails = list(member_emails)
 
-                        logger.debug('Name, description, users: %s, %s, %s',
+                        logger.debug('Name, Description, Users: %s, %s, %s',
                                      name, description, member_emails)
 
                         if not synced_users_dictionary_creation:
                             result = create_stream(
+                                client,
                                 name,
                                 description,
                                 member_emails,
@@ -87,7 +116,7 @@ def main():
 
                             logger.debug('Result: %s', result)
         except Exception as exception:
-            logger.error(exception)
+            logger.error(exception, exc_info=True)
 
         logger.debug('Writing synced users to: %s', synced_users_path)
         with open(synced_users_path, 'wb') as f:
