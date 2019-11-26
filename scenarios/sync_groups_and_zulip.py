a48a97e47e5d99097f42855e5d0190769f5da8c9
Ragnaruk <ima1365@me.com>
2019-11-26 09:03:13 +0300

    Updated script to look for non-default stream names

--- scenarios/sync_groups_and_zulip.py
+++ scenarios/sync_groups_and_zulip.py
@@ -4,7 +4,7 @@ from time import sleep
 from logs.logging import get_logger
 from api_google.google_api_directory import get_directory_service, \
     get_groups_for_domain, get_members_for_group
-from api_zulip.zulip_api import get_client, create_stream, get_all_users
+from api_zulip.zulip_api import get_client, create_stream, get_all_users, get_all_streams
 from config.config import sync_groups_and_zulip, path_data_directory
 
 
@@ -42,10 +42,16 @@ def main():
                 [member['email'] for member in get_all_users(client)['members']]
             )
 
+            zulip_stream_names = sorted(
+                [stream['name'] for stream in get_all_streams(client)['streams']]
+            )
+
             # Get members of Google Groups and remove those who aren't registered in Zulip,
             # then create streams and invite remaining users.
             for group in groups:
                 logger.debug('Group: %s', group)
+
+                name = get_current_stream_name(logger, zulip_stream_names, group['id'])
 
                 # Create a set for the group if it doesn't exist yet
                 if group['email'] not in synced_users:
@@ -73,7 +79,7 @@ def main():
                 if not synced_users_dictionary_creation:
                     result = create_stream(
                         client,
-                        group['name'],
+                        name,
                         group['description'],
                         member_emails,
                         True
@@ -91,6 +97,43 @@ def main():
         logger.info('Update finished. Registered %s users. Sleeping for %s seconds.',
                     number_of_registered_users, sync_groups_and_zulip['sleep_time'])
         sleep(sync_groups_and_zulip['sleep_time'])
+
+
+def get_current_stream_name(logger, zulip_stream_names, stream_id):
+    """
+    Check all Zulip streams and get a name for the current stream.
+    Raise ValueError if it is impossible to narrow down stream names.
+
+    :param logger: Logger object
+    :param zulip_stream_names: List of Zulip stream names
+    :param stream_id: ID of the stream (default name)
+    :return: Name of the current stream
+    """
+    name = stream_id
+    if stream_id not in zulip_stream_names:
+        names_with_id = [x for x in zulip_stream_names if x.startswith(stream_id)]
+
+        logger.debug('Stream names with id: %s', names_with_id)
+
+        if not names_with_id:
+            pass
+        elif len(names_with_id) == 1:
+            name = names_with_id[0]
+        elif len(names_with_id) > 1:
+            names_with_id_and_space = [x for x in names_with_id if x.startswith(stream_id + ' ')]
+
+            logger.debug('names_with_id_and_space: %s', names_with_id_and_space)
+
+            if len(names_with_id_and_space) == 1:
+                name = names_with_id_and_space[0]
+            else:
+                logger.error('Several streams starting with: %s', stream_id)
+                logger.error('Without space: %s', names_with_id)
+                logger.error('With space: %s', names_with_id_and_space)
+
+                raise ValueError
+
+    return name
 
 
 if __name__ == '__main__':
